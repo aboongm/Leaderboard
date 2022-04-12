b2004d08647c8ad15e789f892972bf78fb4cac95
aboongm <aboongm@yahoo.com>
2022-04-12 14:08:54 +0530

    Add webpack, linters, and setup files

---
+++ src/module/constElements.js
@@ -0,0 +1,18 @@
+const refreshTask = document.querySelector('#refresh');
+const taskInput = document.querySelector('#task');
+const submitInput = document.querySelector('#arrow-submit');
+const taskList = document.querySelector('#taskList');
+
+const threeDots = document.querySelector('.three-dots');
+const trash = document.querySelector('.trash');
+const clear = document.querySelector('#clear-completed');
+
+export {
+  refreshTask,
+  taskInput,
+  submitInput,
+  taskList,
+  threeDots,
+  trash,
+  clear,
+};
